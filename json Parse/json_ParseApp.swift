7439ac1166d8b623aab0bf208a2b6b89ed5b106c
Ahmet <ahmetbostancikli@gmail.com>
2025-12-03 02:36:44 +0300

    Initial Commit

---
+++ json Parse/json_ParseApp.swift
@@ -0,0 +1,17 @@
+//
+//  json_ParseApp.swift
+//  json Parse
+//
+//  Created by MrBostan on 3.12.2025.
+//
+
+import SwiftUI
+
+@main
+struct json_ParseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
